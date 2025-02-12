8132ced5dc16e192fb8ad39080c4c44d93c87a2b
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-02-12 22:58:58 +0200

    Update 3.9. Photos Captured on the Tenth Day.sql

--- Regular Exam - 15 June 2024 (My exam)/3.9. Photos Captured on the Tenth Day.sql
+++ Regular Exam - 15 June 2024 (My exam)/3.9. Photos Captured on the Tenth Day.sql
@@ -1,4 +1,3 @@
--- CHAT GPT CORRECT
 SELECT
     CONCAT(SUBSTRING(description FROM 1 FOR 10), '...') AS summary,
 	TO_CHAR(capture_date, 'DD.MM HH24:MI') AS "date"
@@ -8,16 +7,4 @@ WHERE
     EXTRACT(DAY FROM capture_date) = 10
 ORDER BY
 	capture_date DESC
-;
-
--- MY INCORRECT
-SELECT
-    CONCAT(SUBSTRING(description FROM 1 FOR 10), '...') AS summary,
-	TO_CHAR(capture_date, 'DD.MM HH24:MI') AS "date"
-FROM 
-    photos
-WHERE
-    EXTRACT(DAY FROM capture_date) = 10
-ORDER BY
-	"date" DESC
 ;
